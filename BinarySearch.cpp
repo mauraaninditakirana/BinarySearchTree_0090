c3c8d655eec3b94b8a4113690ac557d0ced78a44
mauraaninditakirana <kiranamaura351@gmail.com>
2024-06-14 14:06:00 +0700

    menambahkan if dalam void insert untuk jika induk null, maka tree kosong, dengan menandai node baru sebagai root kemudian jika nilai dalam node baru lebih kecil dari induk maka leftchild dari induk menunjuk ke node baru, namun jika lebih besar, maka rightchild dari induk menunjuk ke node baru

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -36,6 +36,19 @@ public:
         Node* currentNode = NULL;
         search(element, parent, currentNode); //locate the node which will be the parent of the node to be inserted
 
+        if (parent == NULL) //if the parent is NULL (treeis empty)
+        {
+            ROOT = newNode; //mark the new node as ROOT
+            return; //exit
+        }
+        if (element < parent-> info) //if the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
+        }
+        else if(element > parent -> info ) //if the value in the data field of the new node is greater than that of the parent
+        {
+            parent -> rightchild = newNode; // make the right child of the parent point to the new node 
+        }
         
     }
 };
