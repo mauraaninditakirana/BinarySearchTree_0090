5990f1d6e492743cde3c8575021f043b38dd5340
mauraaninditakirana <kiranamaura351@gmail.com>
2024-06-14 13:55:46 +0700

    menambahkan void insert untuk menambahkan node kedalam binary searchtree, dengan mengalokasi memeori untuk node baru, kmudian ditetapkan nilai dalam node baru tsbut, dilanjut dengan membuat leftrightchild dari node baru yang menunjuk ke null, kemudian menggunakan search untuk mengalokasikan node yang akan menjadi induk dari node baru yang akan ditambahkan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -25,4 +25,17 @@ public:
     {
         ROOT = NULL; // initializing ROOT to NULL 
     }
+    void insert(string element) // insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, NULL, NULL); //allocate memory for the new node
+        newNode->info = element; //Assign value to the data field of the new node
+        newNode->leftchild = NULL; // make the left child of the new node point to null
+        newNode->rightchild = NULL; // make the right child of the new node point to null
+
+        Node* parent = NULL;
+        Node* currentNode = NULL;
+        search(element, parent, currentNode); //locate the node which will be the parent of the node to be inserted
+
+        
+    }
 };
