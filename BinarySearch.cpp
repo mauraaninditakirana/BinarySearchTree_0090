260193b2fef5e142fd211b40c9cbbf00bd3c4bfd
mauraaninditakirana <kiranamaura351@gmail.com>
2024-06-14 14:14:29 +0700

    membuat void search untuk mencari currentNode saat ini dari currentnode yang ditentukan, serta node saatini dari induk

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -51,4 +51,18 @@ public:
         }
         
     }
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        //this function searches the currentnode of the specified node as well as the current node of its parent
+        currentNode = ROOT;
+        parent = NULL;
+        while((currentNode != NULL) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element<currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
 };
